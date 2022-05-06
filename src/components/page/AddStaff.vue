2675339ad551eeeae343367b64bbfce8c6935b4b
khoa.nguyen <khoanguyen190299@gmail.com>
2022-05-06 17:16:40 +0700

    update add & delete staff

--- src/components/page/AddStaff.vue
+++ src/components/page/AddStaff.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="staff">
+    <div class="toast-success" :class="!isShowToats? 'hide-toast' : 'show-toast'">Thêm thành công</div>
     <div class="staff-header w-100">
       <h6 class="staff-title mb-0">Thêm nhân viên</h6>
     </div>
@@ -17,47 +18,47 @@
             <div class="col-md-6">
               <div>
                 <label class="fw-500">Họ và tên</label>
-                <input type="text" class="form-control fs-12" placeholder="Ví dụ: Nguyễn Văn A">
+                <input type="text" class="form-control fs-12" placeholder="Ví dụ: Nguyễn Văn A" v-model="formStaff.name">
               </div>
               <div class="mt-2">
                 <label class="fw-500">Tuổi</label>
-                <input type="text" class="form-control fs-12" placeholder="Nhập tuổi...">
+                <input type="text" class="form-control fs-12" placeholder="Nhập tuổi..." v-model="formStaff.age">
               </div>
               <div class="mt-2">
                 <label class="fw-500">Số điện thoại</label>
-                <input type="text" class="form-control fs-12" placeholder="Nhập số điện thoại...">
+                <input type="text" class="form-control fs-12" placeholder="Nhập số điện thoại..." v-model="formStaff.phone">
               </div>
               <div class="mt-2">
                 <label class="fw-500">Email</label>
-                <input type="text" class="form-control fs-12" placeholder="Nhập email...">
+                <input type="text" class="form-control fs-12" placeholder="Nhập email..." v-model="formStaff.email">
               </div>
             </div>
             <div class="col-md-6">
               <div>
                 <label class="fw-500">Bộ phận</label>
-                <b-form-select :options="lang" class="form-control fs-12"></b-form-select>
+                <b-form-select :options="lang" class="form-control fs-12" v-model="formStaff.lang"></b-form-select>
               </div>
               <div class="mt-2">
                 <label class="fw-500">Thời gian tuyển</label>
-                <input type="date" class="form-control fs-12" value="05/03/2022">
+                <input type="date" class="form-control fs-12" v-model="formStaff.time">
               </div>
               <div class="mt-2">
                 <label class="fw-500">Địa chỉ</label>
-                <input type="text" class="form-control fs-12" placeholder="Nhập địa chỉ...">
+                <input type="text" class="form-control fs-12" placeholder="Nhập địa chỉ..." v-model="formStaff.address">
               </div>
             </div>
             <div class="w-100">
               <div class="mt-2">
                 <label class="fw-500">Thông tin thêm</label>
-                <textarea class="w-100 form-control fs-12"></textarea>
+                <textarea class="w-100 form-control fs-12" v-model="formStaff.moreInfo"></textarea>
               </div>
             </div>
           </div>
         </div>
         <div class="d-flex justify-content-end mt-3">
           <div class="d-flex gap-2">
-            <button class="btn btn-sm btn-secondary fw-500">Hủy</button>
-            <button class="btn btn-sm btn-success fw-500">Thêm</button>
+            <button class="btn btn-sm btn-secondary fw-500" @click="cancelAddStaff">Hủy</button>
+            <button class="btn btn-sm btn-success fw-500" @click="addStaff">Thêm</button>
           </div>
         </div>
       </div>
@@ -66,11 +67,43 @@
 </template>
 
 <script>
+import router from "@/router";
+
 export default {
   name: "AddStaff",
   data() {
     return {
-      lang: ['Java','Java','Java','Java','Java']
+      isShowToats: false,
+      lang: ['Java','Java','Java','Java','Java'],
+      formStaff: {}
+    }
+  },
+  methods: {
+    cancelAddStaff() {
+      router.go(-1)
+    },
+    addStaff() {
+      const data = this.formStaff
+      fetch('https://62674f9201dab900f1bd5a5c.mockapi.io/staff/staff', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+          .then(response => response.json())
+          .then(data => {
+            console.log('Success:', data);
+            this.isShowToats = true
+            var me = this
+            setTimeout(function () {
+              me.isShowToats = false
+            }, 3000)
+
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
     }
   }
 }
--- src/components/page/ListStaff.vue
+++ src/components/page/ListStaff.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="staff">
+    <div class="toast-success" :class="!isShowToats? 'hide-toast' : 'show-toast'">Xóa thành công</div>
     <div class="staff-header w-100">
       <h6 class="staff-title mb-0">Danh sách nhân viên</h6>
     </div>
@@ -10,13 +11,66 @@
             <button class="btn btn-sm btn-outline-secondary">Import</button>
             <button class="btn btn-sm btn-outline-secondary ms-3">Export</button>
           </div>
-          <div class="search mb-3">
-            <input class="form-control input-search fs-14" type="search" placeholder="Tìm kiếm...">
-            <img class="icon-search" src="../../assets/images/icons/search.png">
+          <div class="d-flex">
+            <div class="search mb-3">
+              <input class="form-control input-search fs-14" type="search" placeholder="Tìm kiếm...">
+              <img class="icon-search" src="../../assets/images/icons/search.png">
+            </div>
+            <button class="btn btn-sm btn-success mb-3 ms-2" title="Thêm nhân viên" @click="addStaff">Thêm</button>
           </div>
+
         </div>
         <div class="table-box">
-          <b-table
+          <div class="overflow-auto w-100 mb-0" v-if="this.staffs.length">
+            <div class="table-heading fw-500 d-flex w-100 m-0">
+              <div class="table-title th-class-1 border-start-0">Chọn</div>
+              <div class="table-title th-class-2">Mã</div>
+              <div class="table-title th-class-2 justify-content-center">Ảnh</div>
+              <div class="table-title th-class-4">Họ và tên</div>
+              <div class="table-title th-class-1">Tuổi</div>
+              <div class="table-title th-class-4">Số điện thoại</div>
+              <div class="table-title th-class-4">Email</div>
+              <div class="table-title th-class-4">Address</div>
+              <div class="table-title th-class-3">Bộ phận</div>
+              <div class="table-title action th-class-3 d-flex justify-content-center">Action</div>
+            </div>
+            <div
+                class="table-item d-flex fs-14"
+                v-for="staff in staffs"
+                :key="staff.id"
+            >
+              <div class="table-content th-class-1 border-start-0 justify-content-center">
+                <input type="checkbox">
+              </div>
+              <div class="table-content th-class-2">HN{{ staff.id }}</div>
+              <div class="table-content th-class-2 justify-content-center">
+                <div class="img-staff p-0">
+                  <img class="w-100" :src="staff.image" alt=" " />
+                </div>
+              </div>
+              <div class="table-content th-class-4">{{ staff.name }}</div>
+              <div class="table-content th-class-1">{{ staff.age }}</div>
+              <div class="table-content th-class-4">{{ staff.phone }}</div>
+              <div class="table-content th-class-4">{{ staff.email }}</div>
+              <div class="table-content th-class-4">{{ staff.address }}</div>
+              <div class="table-content th-class-3">{{ staff.lang }}</div>
+              <div class="table-content bg-white action th-class-3">
+                <div class="action-item d-flex justify-content-center m-auto">
+                  <div @click="editStaff" title="Edit" class="me-3 icon-action">
+                    <img class="w-100" src="../../assets/images/icons/editing.png">
+                  </div>
+                  <div @click="deleteStaff(staff)" title="Delete" class="icon-action">
+                    <img class="w-100" src="../../assets/images/icons/bin.png">
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div class="text-center mt-5 mb-5" v-else>
+            <h5 class="not-staff">Hiện tại chưa có nhân viên nào!</h5>
+          </div>
+<!--          <b-table
               class="table-content m-0"
               :items="staffItems"
               :fields="staffTitle"
@@ -30,12 +84,17 @@
                 <div @click="editStaff" title="Edit" class="me-3 icon-action">
                   <img class="w-100" src="../../assets/images/icons/editing.png">
                 </div>
-                <div @click="deleteStaff" title="Delete" class="icon-action">
+                <div @click="row.deleteStaff" title="Delete" class="icon-action">
                   <img class="w-100" src="../../assets/images/icons/bin.png">
                 </div>
               </div>
             </template>
-          </b-table>
+            <template #cell(image)>
+              <div class="img-staff">
+                <img class="w-100" :src="staffItems.image" alt=" ">
+              </div>
+            </template>
+          </b-table>-->
         </div>
       </div>
     </div>
@@ -49,7 +108,7 @@ export default {
   components: {},
   data() {
     return {
-      staffTitle: [
+      /*staffTitle: [
         {
           key: 'id',
           sortable: false,
@@ -104,8 +163,9 @@ export default {
           thClass: 'th-class-3 table-title action',
           tdClass: 'action bg-white p-0',
         },
-      ],
-      staffItems: [],
+      ],*/
+      staffs: [],
+      isShowToats: false,
     }
   },
   created () {
@@ -115,14 +175,38 @@ export default {
     async getData() {
       const response = await fetch("https://62674f9201dab900f1bd5a5c.mockapi.io/staff/staff")
       const data = await response.json()
-      this.staffItems = data
+      this.staffs = data
+    },
+    addStaff() {
+      router.push('/hcpm-app-042022/home/add-staff/')
     },
     editStaff() {
       router.push('/hcpm-app-042022/home/list-staff/edit-staff/')
     },
-    deleteStaff() {
-
-    }
+    deleteStaff(staff) {
+      console.log(staff)
+      const data = this.formStaff
+      fetch('https://62674f9201dab900f1bd5a5c.mockapi.io/staff/staff/' + staff.id, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+          .then(response => response.json())
+          .then(data => {
+            console.log('Success:', data);
+            this.getData()
+            this.isShowToats = true
+            var me = this
+            setTimeout(function () {
+              me.isShowToats = false
+            }, 3000)
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
+    },
   }
 }
 </script>
